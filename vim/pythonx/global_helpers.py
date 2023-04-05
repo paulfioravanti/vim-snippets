1bf70d8212b94e0d0f8b39043b57d60bce6b9110
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-04-05 14:11:44 +1000

    Clean up

--- vim/pythonx/global_helpers.py
+++ vim/pythonx/global_helpers.py
@@ -15,6 +15,14 @@ _CLOSING_CHARACTERS = {
     "\"": "\""
 }
 
+def choice_tabstop_chosen(choice_tabstop):
+    """
+    Checks if a choice tabstop has been chosen.
+    """
+    # `choice_tabstop` comes through as a string. If it starts with "1.", then
+    # a choice has not been made yet.
+    return not choice_tabstop.startswith("1.")
+
 def closing_character(tabstop):
     """
     Return closing character for a tabstop containing an opening character.
@@ -33,20 +41,18 @@ def key_closing(opening):
     """
     return "\" =>" if opening == "\"" else ":"
 
-def choice_tabstop_chosen(choice_tabstop):
-    """
-    Checks if a choice tabstop has been chosen.
-    """
-    # `choice_tabstop` comes through as a string. If it starts with "1.", then
-    # a choice has not been made yet.
-    return not choice_tabstop.startswith("1.")
-
 def jump_forward():
     """
     Jumps forward a tabstop.
     """
     # REF: https://github.com/reconquest/vim-pythonx/blob/master/pythonx/px/snippets.py#LL117C5-L118C41
     vim.command('call feedkeys("\<C-R>=UltiSnips#JumpForwards()\<CR>")')
+
+def maybe(tabstop, default):
+    """
+    A simple maybe clause.
+    """
+    return "" if tabstop else default
 
 def maybe_comma(left, right):
     """
@@ -60,4 +66,4 @@ def maybe_surround(choice_tabstop, surround):
     Surround a choice tabstop with `surround` chars to indicate the whole
     tabstop itself is optional.
     """
-    return surround if not choice_tabstop_chosen(choice_tabstop) else ""
+    return "" if choice_tabstop_chosen(choice_tabstop) else surround
