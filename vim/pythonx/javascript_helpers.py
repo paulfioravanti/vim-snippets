1022ce2a2988ad302b0bdb5a2bf8226207a98af5
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-06-02 10:57:32 +1000

    Fix bug

--- vim/pythonx/javascript_helpers.py
+++ vim/pythonx/javascript_helpers.py
@@ -11,5 +11,5 @@ def get_config_option(snip, option, default=None):
     return snip.opt('g:ultisnips_javascript["{}"]'.format(option), default)
 
 def maybe_semi(snip):
-    option = get_option(snip, "semi", "always")
+    option = get_config_option(snip, "semi", "always")
     return ";" if option == "always" else ""
