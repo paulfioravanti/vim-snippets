2bc31b301ac1f0eab2df38da3b1b2d9d350b61a9
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-07-07 10:07:47 +1000

    Extract out class_attribute_name into the JavaScript helper file

--- vim/pythonx/javascript_helpers.py
+++ vim/pythonx/javascript_helpers.py
@@ -4,10 +4,23 @@ Based off https://github.com/honza/vim-snippets/blob/master/pythonx/javascript_s
 NOTE: Changes to this file require restarting Vim!
 """
 import re
+from UltiSnips import vim_helper
 
 
 # REF: https://stackoverflow.com/a/3469155/567863
 _NON_NEWLINE_WHITESPACE = "[^\S\r\n]"
+
+def class_attribute_name():
+    """
+    Determines the class attribute name of a tag based on the file type.
+    This handling is needed specifically for React.
+    """
+    # NOTE: snip.ft not available during pre_expand/post_jump, which is when
+    # this function is used (html.snippets).
+    # See SnippetUtilForAction class in Ultisnips codebase to show that it only
+    # responds to `expand_anon`:
+    # https://github.com/SirVer/ultisnips/blob/master/pythonx/UltiSnips/text_objects/python_code.py
+    return "className" if vim_helper.eval("&filetype") == "javascript" else "class"
 
 def maybe_semi(snip):
     # NOTE: This guard is needed to prevent recursion bugs when starting a
