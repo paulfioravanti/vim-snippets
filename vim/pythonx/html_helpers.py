bace335305710503d34b42b27a966e156d175441
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-07-07 10:45:02 +1000

    Expose HTML helper constants through functions rather than import them directly

--- vim/pythonx/html_helpers.py
+++ vim/pythonx/html_helpers.py
@@ -44,7 +44,7 @@ _HTML_TAG_KEYWORDS = [
     "var",
     "video"
 ]
-HTML_TAG_KEYWORD = re.compile(rf"\b({'|'.join(_HTML_TAG_KEYWORDS)})\b")
+_HTML_TAG_KEYWORD = re.compile(rf"\b({'|'.join(_HTML_TAG_KEYWORDS)})\b")
 
 _HTML_INPUT_TYPE_KEYWORDS = [
     "button",
@@ -65,20 +65,20 @@ _HTML_INPUT_TYPE_KEYWORDS = [
     "url",
     "week",
 ]
-HTML_INPUT_TYPE_KEYWORD = re.compile(
+_HTML_INPUT_TYPE_KEYWORD = re.compile(
     rf"\b({'|'.join(_HTML_INPUT_TYPE_KEYWORDS)})\b"
 )
 
 _HTML_MIN_MAX_INPUT_TYPE_KEYWORDS = ["date", "number", "range"]
-HTML_MIN_MAX_INPUT_TYPE_KEYWORD = re.compile(
+_HTML_MIN_MAX_INPUT_TYPE_KEYWORD = re.compile(
     rf"\b({'|'.join(_HTML_MIN_MAX_INPUT_TYPE_KEYWORDS)})\b"
 )
 
-IDS = re.compile(r"(?<=#)[^\.#]+") # #some-id
-CLASSES = re.compile(r"(?<=\.)[^\.#]+") # .some-class
+_IDS = re.compile(r"(?<=#)[^\.#]+") # #some-id
+_CLASSES = re.compile(r"(?<=\.)[^\.#]+") # .some-class
 
 # Bridging list of English words/phrases to HTML tag names
-TAG_NAME_MAPPINGS = {
+_TAG_NAME_MAPPINGS = {
     "figure caption": "figcaption",
     "heading 1": "h1",
     "heading 2": "h2",
@@ -98,3 +98,48 @@ TAG_NAME_MAPPINGS = {
     "text area": "textarea",
     "unordered list": "ul"
 }
+
+def can_map_to_tag_name(string):
+    """
+    Checks whether string can be mapped to a HTML tag.
+    """
+    return string in _TAG_NAME_MAPPINGS
+
+def extract_classes(attributes):
+    """
+    Extract html classes from a string of attributes.
+    """
+    return _attribute_text(_CLASSES, attributes)
+
+def extract_ids(attributes):
+    """
+    Extract html ids from a string of attributes.
+    """
+    return _attribute_text(_IDS, attributes)
+
+def is_html_input_tag(string):
+    """
+    Checks whether string contains a HTML input tag with a known input type.
+    """
+    return re.match(_HTML_INPUT_TYPE_KEYWORD, string)
+
+def is_html_min_max_input_tag(string):
+    """
+    Checks whether string contains a HTML input tag with a known input type.
+    """
+    return re.match(_HTML_MIN_MAX_INPUT_TYPE_KEYWORD, string)
+
+def is_html_tag(string):
+    """
+    Checks whether string contains a known HTML tag.
+    """
+    return re.match(_HTML_TAG_KEYWORD, string)
+
+def text_to_tag(text):
+    """
+    Returns HTML tag from English text or just the text if it cannot be mapped.
+    """
+    return _TAG_NAME_MAPPINGS.get(text, text)
+
+def _attribute_text(attribute_type, attributes):
+    return " ".join(re.findall(attribute_type, attributes))
