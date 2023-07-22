df4f3512f1ad2a31dcffdafc1b8705b947ed2121
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-07-22 22:31:07 +1000

    Elm maybe_text helper function wasn't working out, so remove it

--- vim/pythonx/elm_helpers.py
+++ vim/pythonx/elm_helpers.py
@@ -2,26 +2,15 @@
 Elm-related Ultisnips snippet helper functions.
 NOTE: Changes to this file require restarting Vim!
 """
-# ~/.vim/pythonx/global_helpers.py
-from global_helpers import (
-    choice_tabstop_chosen
-)
-
 
 def flags_text(tabstop):
     """
     Return default text for flags
     """
-    if not choice_tabstop_chosen(tabstop):
-        return "flags"
-
-    return tabstop.lower()
-
-def maybe_flags(tabstop):
-    """
-    Return default flags text or remove argument
-    """
-    if not choice_tabstop_chosen(tabstop):
-        return " flags"
-
-    return "" if tabstop == "()" else f" {tabstop.lower()}"
+    match tabstop:
+        case "Flags":
+            return "flags"
+        case "()":
+            return "()"
+        case _:
+            return "flags"
