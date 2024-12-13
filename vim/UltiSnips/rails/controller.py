506ffce1b05a27c50e63ca32810dc973b740417a
Paul Fioravanti <paul.fioravanti@gmail.com>
2024-12-13 11:52:31 +1100

    Extract python code from Ruby/Rails snippets

---
+++ vim/UltiSnips/rails/controller.py
@@ -0,0 +1,52 @@
+"""
+Companion python file for `c/stdio.snippets` file.
+"""
+
+# ~/.vim/pythonx/global_helpers.py
+from global_helpers import (
+    snake_to_camel
+)
+
+
+# Cannot type hint a `snip`.
+def controller_class_resource_name(snip):
+    """
+    Provide default name for a resource in a Rails controller class.
+    """
+    return snake_to_camel(controller_file_resource_name(snip))
+
+# Cannot type hint a `snip`.
+def controller_file_resource_name(snip):
+    """
+    Provide default name for a resource from a Rails controller file.
+    """
+    return snip.basename.replace("_controller", "")
+
+def controller_model_default(controller_variable: str) -> str:
+    """
+    Provide default name for a model in a Rails controller file.
+    """
+    return snake_to_camel(controller_variable)
+
+# Cannot type hint a `snip`.
+def controller_singular_variable(snip):
+    """
+    Provide default name for a singular variable in a Rails controller file.
+    """
+    controller_resource_name = controller_file_resource_name(snip)
+    if controller_resource_name.endswith("s"):
+        return controller_resource_name[:-1]
+
+    # Plural does not end in "s", and we do not have a plural/singular
+    # dictionary to work with, so just return the plural name and correct it
+    # within the snippet inline.
+    return controller_resource_name
+
+def params_id(controller_name: str, variable: str, model_name: str) -> str:
+    """
+    Provide default name for the params id in a Rails controller file.
+    """
+    if model_name == controller_name.rstrip("s"):
+        return "id"
+
+    return variable + "_id"
---
+++ vim/UltiSnips/ruby/object.py
@@ -0,0 +1,13 @@
+"""
+Companion python file for `ruby/object.snippets` file.
+"""
+
+from typing import Match
+
+
+def method_or_proc(match: Match):
+    """
+    Determine whether to provide default text for a method or proc based on
+    match object data.
+    """
+    return "." if match.group(1) == "." else " &"
