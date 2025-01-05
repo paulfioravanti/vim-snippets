e24e0701036463709b88b8d79b4555204c4ed8a2
Paul Fioravanti <paul.fioravanti@gmail.com>
2025-01-05 16:24:23 +1100

    Merge python_helpers python file with python.py

--- vim/pythonx/python.py
+++ vim/pythonx/python.py
@@ -1,9 +1,22 @@
 """
-Companion python file for `python.snippets` file.
+Companion python file for all Python snippets.
 NOTE: Changes to this file require restarting Vim!
 """
 
 from typing import Match
+
+# ~/.vim/pythonx/globals.py
+from globals import (
+    snake_to_camel
+)
+
+
+# Cannot type hint a `snip`.
+def class_name(snip):
+    """
+    Convert snippet file basename to camel case.
+    """
+    return snake_to_camel(snip.basename)
 
 def comprehension_opening(match: Match) -> str:
     """
